c09a9c4150493e6d4a2f69edcab5162abd76dc13
HaroldIB <haroldbustamante22@gmail.com>
2022-12-03 17:48:26 -0400

    second commit

---
+++ resources/views/layout/navbarsearch.blade.php
@@ -0,0 +1,14 @@
+<!-- navbar search -->
+<div id="navbar-search" class="navbar-search collapse">
+<h1 style="color: black; background-color: red;">NOMBRE: HAROLD BUSTAMANTE CI: 7026792</h1>
+    <div class="navbar-search-inner">
+        <form action="#">
+            <span class="search-icon"><i class="fa fa-search"></i></span>
+            <input class="search-field" type="search" placeholder="search..."/>
+        </form>
+        <button type="button" class="search-close" data-toggle="collapse" data-target="#navbar-search" aria-expanded="false">
+            <i class="fa fa-close"></i>
+        </button>
+    </div>
+    <div class="navbar-search-backdrop" data-toggle="collapse" data-target="#navbar-search" aria-expanded="false"></div>
+</div><!-- .navbar-search -->
